86bfced9286464964ff5cf86150b469afacf0cb5
Suzuha Kiuchi <kiuchi.ju@gmail.com>
2025-01-07 23:55:59 +0900

    add test

---
+++ test/test.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+timeout 10 ros2 launch cpu_usage_ros2 usage.launch.py | tee -i /tmp/cpu_usage_ros2.log
+
+cat /tmp/cpu_usage_ros2.log |
