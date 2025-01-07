4b08c0c8cea27c02708b4e7f64aa36c11655ed59
Suzuha Kiuchi <kiuchi.ju@gmail.com>
2025-01-07 23:31:30 +0900

    add launch

---
+++ launch/usage.launch.py
@@ -0,0 +1,14 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+
+def generate_launch_description():
+
+    cpu_usage = launch_ros.actions.Node(
+            package='cpu_usage_ros2',
+            executable='cpu_usage',
+            )
+
+    return launch.LaunchDescription([cpu_usage])
