7b38d2960bd443788a0c9a705de98ee58b062f6b
Suzuha Kiuchi <kiuchi.ju@gmail.com>
2025-01-08 00:46:59 +0900

    test修正

--- test/test.bash
+++ test/test.bash
@@ -8,4 +8,4 @@ colcon build
 source $dir/.bashrc
 timeout 10 ros2 launch cpu_usage_ros2 usage.launch.py | tee -i /tmp/cpu_usage_ros2.log
 
-cat /tmp/cpu_usage_ros2.log |
+cat /tmp/cpu_usage_ros2.log 
